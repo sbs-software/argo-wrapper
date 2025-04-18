6d4481a923bfc46f4b7bbe698b61a482ae8cb4d9
fstaudt <fstaudt@gmail.com>
2025-04-18 16:54:27 +0200

    build: bump Spring Boot to 3.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.1"
+    id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.10.4"
 }
